0598f34440c4e641d6d6876330524204fad35d43
potatodev15 <d_hrnslv@proton.me>
2024-03-08 13:57:40 +0200

    new-commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="relative bg-cover bg-center min-h-screen flex items-center justify-center text-white">
-        <div className="absolute inset-0 bg-black opacity-50">
-            <div className="bg-[url('https://picsum.photos/1920/1080')] absolute inset-0 opacity-20"></div>
-            </div>
-        <div className="text-center z-10">
-         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6">Your Heading Here</h1>
-         <p className="text-lg md:text-xl lg:text-2xl mb-12">Your description text goes here</p>
-         <button className="px-6 py-3 text-lg md:text-xl lg:text-2xl bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">Button Text</button>
+    <div className="relative bg-cover bg-center h-[90vh] flex items-center justify-center text-white">
+      <div className="absolute inset-0 h-full bg-black opacity-50">
+        <div className="bg-[url('https://picsum.photos/1920/1080')] absolute inset-0 opacity-20"></div>
+      </div>
+      <div className="text-center z-10 mt-10">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-4 md:mb-6 lg:mb-8">Your Heading Here</h1>
+        <p className="text-sm md:text-base lg:text-lg xl:text-xl mb-6 md:mb-8">Your description text goes here</p>
+        <button className="px-4 md:px-6 py-2 md:py-3 text-base md:text-lg lg:text-xl bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">Button Text</button>
+      </div>
     </div>
-</div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,39 +17,41 @@ const Navbar = () => {
     ];
 
     return (
-        <div className='bg-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-blue-500'>
-            <img src="https://picsum.photos/64" alt="" />
+        <div className='w-full h-19 bg-white bg-default fixed z-[3]'>
+            <div className='max-w-[1240px] mx-auto flex flex-row justify-between items-center'>
+                <img src="https://picsum.photos/64" alt="" />
 
-            {/* Desktop Navigation */}
-            <ul className='hidden md:flex'>
-                {navItems.map(item => (
-                    <a href={item.href} key={item.id} className="block">
-                        <li className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'>
-                            {item.text}
-                        </li>
-                    </a>
-                ))}
-            </ul>
+                {/* Desktop Navigation */}
+                <ul className='hidden md:flex'>
+                    {navItems.map(item => (
+                        <a href={item.href} key={item.id} className="block">
+                            <li className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'>
+                                {item.text}
+                            </li>
+                        </a>
+                    ))}
+                </ul>
 
-            <div onClick={handleNav} className='block md:hidden'>
-                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                <div onClick={handleNav} className='block md:hidden'>
+                    {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                </div>
+
+                <ul
+                    className={
+                        nav
+                            ? 'z-[999] text-center fixed md:hidden left-0 top-0 w-[60%] h-full border-r white bg-white ease-in-out duration-500'
+                            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+                    }
+                >
+                    <img src="https://picsum.photos/128" alt="" className='m-4 mx-auto rounded-full'/>
+
+                    {navItems.map(item => (
+                        <a href={item.href} className="block">
+                            <li className='p-4 cursor-pointer'>{item.text}</li>
+                        </a>
+                    ))}
+                </ul>
             </div>
-
-            <ul
-                className={
-                    nav
-                        ? 'z-[999] text-center fixed md:hidden left-0 top-0 w-[60%] h-full border-r white bg-white ease-in-out duration-500'
-                        : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
-                }
-            >
-                <img src="https://picsum.photos/128" alt="" className='m-4 mx-auto rounded-full'/>
-
-                {navItems.map(item => (
-                    <a href={item.href} className="block">
-                        <li className='p-4 cursor-pointer'>{item.text}</li>
-                    </a>
-                ))}
-            </ul>
         </div>
     );
 };
